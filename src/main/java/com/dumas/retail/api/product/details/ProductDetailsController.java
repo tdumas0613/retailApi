76643184a52622d77c2fba274a475e0df4b7c566
Taylor Dumas <taylorjdumas@gmail.com>
2019-06-24 21:31:57 -0500

    remove unused imports

--- src/main/java/com/dumas/retail/api/product/details/ProductDetailsController.java
+++ src/main/java/com/dumas/retail/api/product/details/ProductDetailsController.java
@@ -1,7 +1,6 @@
 package com.dumas.retail.api.product.details;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
