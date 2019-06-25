f5b22527f0e9e8edb7a8dbad9f08e068e19cbee7
Taylor Dumas <taylorjdumas@gmail.com>
2019-06-24 20:53:05 -0500

    remove unused imports from MyRetailApplication.java

--- src/main/java/com/dumas/retail/api/MyRetailApplication.java
+++ src/main/java/com/dumas/retail/api/MyRetailApplication.java
@@ -3,10 +3,6 @@ package com.dumas.retail.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import com.mongodb.MongoClient;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.dumas.retail.api"})
